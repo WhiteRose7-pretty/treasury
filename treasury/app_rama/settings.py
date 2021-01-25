453e529b46821056ad0fe10595b85e562bfd7a57
Ju Gui <cholpine@outlook.com>
2021-01-25 08:39:01 +0100

    fix feedback

--- treasury/app_rama/settings.py
+++ treasury/app_rama/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'pl'
+
 
 TIME_ZONE = 'UTC'
 
@@ -121,7 +121,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '../static/'
+
 
 MEDIA_URL = '/media/'
 

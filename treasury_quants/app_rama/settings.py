30abbe9fc4d378c5a2bf41f7b78b38a16bbda0b8
Ju Gui <cholpine@outlook.com>
2021-02-23 18:11:59 +0100

    add middleware

--- treasury_quants/app_rama/settings.py
+++ treasury_quants/app_rama/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'account_cycle.middleware.ProcessViewNoneMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -108,6 +109,8 @@ CELERY_BROKER_URL = 'pyamqp://{user}:{password}@{host}/{vhost}'.format(
     host='rabbitmq',
     vhost='/'
 )
+
+# CELERY_BROKER_URL = 'amqp://localhost'
 
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_ACCEPT_CONTENT = ['json', 'application/json']
